796eadb8cecc57e964d65fdd2c81d48a55ab6544
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-10-31 14:10:49 -0500

    Adds Required Worlds
    According to the pre-production slideshow, adds all six required worlds for the game. The sixth, "Game_Select" should not be added until the proper implementation of it is discussed with Mr. Monroe.

---
+++ Astrorunners/Astrorunners.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Astrorunners here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Astrorunners extends World
+{
+
+    /**
+     * Constructor for objects of class Astrorunners.
+     * 
+     */
+    public Astrorunners()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 960, 1); 
+    }
+}
---
+++ Astrorunners/Round_End.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Round_End here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Round_End extends World
+{
+
+    /**
+     * Constructor for objects of class Round_End.
+     * 
+     */
+    public Round_End()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 960, 1); 
+    }
+}
