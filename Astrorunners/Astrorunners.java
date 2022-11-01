958b32176941f519704e02f11a3e4153d253f979
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-11-01 14:28:40 -0500

    Corrects extraneous comments

--- Astrorunners/Astrorunners.java
+++ Astrorunners/Astrorunners.java
@@ -1,15 +1,9 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 public class Astrorunners extends World
 {
-
-    /**
-     * Constructor for objects of class Astrorunners.
-     * 
-     */
     public Astrorunners()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    {
         super(1280, 960, 1); 
     }
 }
--- Astrorunners/Controls.java
+++ Astrorunners/Controls.java
@@ -1,18 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Controls here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Controls extends World
 {
-
-    /**
-     * Constructor for objects of class Controls.
-     * 
-     */
     public Controls()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
