1ee510e695672bbef6002b0cec1256d522a5c375
Em1lisGod42069 <115831795+Em1lisGod42069@users.noreply.github.com>
2022-11-02 13:50:13 -0500

    Platform added that collides with temp player on y axis

--- Astrorunners/TempActor.java
+++ Astrorunners/TempActor.java
@@ -1,6 +1,15 @@
 import greenfoot.*; 
 public class TempActor extends Actor
 {
+     private static int XCord;
+    private static int YCord;
+    public int getXCord(){
+      return XCord;    
+    }
+    public void SetXCord(){
+        
+    }
+     
     public void act() 
     {  
         int gravity = 5;
@@ -8,6 +17,7 @@ public class TempActor extends Actor
         if(Greenfoot.isKeyDown("D")){setLocation(getX()+5, getY());}
         if(Greenfoot.isKeyDown("A")){setLocation(getX()-5,getY());}
         setLocation(getX(), getY()+gravity);
+        if(isTouching(Platforms.class)){setLocation(getX(),getY()-gravity);}
         //I'm going to keep myself safe :3
-    }    
+    }   
 }
