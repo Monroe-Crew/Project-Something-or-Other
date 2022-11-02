cce9459ea2528606916d2e122cf229ee21284c01
CadenWonzer <97636285+CadenWonzer@users.noreply.github.com>
2022-11-02 16:08:32 -0500

    Black Hole Interaction
    sucks  you to black hole and stuff kinda doresn't wir==ork and is funjy

--- Astrorunners/Astronaut.java
+++ Astrorunners/Astronaut.java
@@ -1,28 +1,40 @@
 import greenfoot.*;
+import java.util.*;
 public class Astronaut extends Actor{
-    private static double velocity;
+    private static double velocityX, velocityY;
     private double x, y;
     public Astronaut(double velocity){
-        this.velocity = 0;
+        this.velocityX = 0;
+        this.velocityY = 0;
     }
+
     public void addedToWorld(World world) {
         x = getX();
         y = getY();
     }
+
     public void act() {
-        
-        velocity += Constants.GRAVITY;
-        y += velocity;
+        List<BlackHole> blackholes = getObjectsInRange(2000, BlackHole.class);
+        if(blackholes.size() > 0) {
+            BlackHole b = blackholes.get(0);
+            int bx = b.getX();
+            int by = b.getY();
+            double xDif = bx - getX();
+            double yDif = by - getY();
+            velocityX=1.0/xDif;
+            velocityY=1.0/yDif;
+            System.out.println(xDif +" " + yDif);
+            //System.out.println(velocityX+" "+velocityY);
+        }
+        x+=velocityX;
+        y+=velocityY;
         setLocation((int)Math.round(x), (int)Math.round(y));
-        
-        
-        
-        //velocity = gravityAcceleration(Constants.GRAVITY, velocity);
-        //gravity(velocity);
     }
+
     public double gravityAcceleration(double gravity, double velocity){
         return velocity + gravity;
     }
+
     public void gravity(double velocity){
         setLocation(getX(), getY() + (int)velocity);
     }
--- Astrorunners/Constants.java
+++ Astrorunners/Constants.java
@@ -1,5 +1,4 @@
 public class Constants{
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 960;
-    public static final double GRAVITY = 0.2;
 }
