778d133a9a9dc9cb1f03bb1974f976f6f2dcf87d
Em1lisGod42069 <115831795+Em1lisGod42069@users.noreply.github.com>
2022-11-03 14:20:29 -0500

    Complete Revamp of the Collision Code and Some movement code
    -Friction added
    -Speed added
    -Jump checking added
    -Collisions revamped

--- Astrorunners/Platforms.java
+++ Astrorunners/Platforms.java
@@ -1,19 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Platforms here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Platforms extends Actor
 {
-    /**
-     * Act - do whatever the Platforms wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public static int PlatformXcord;
+    public static int PlatformYcord;
     public void act() 
     {
-        
+        PlatformXcord = getX();
+        PlatformYcord = getY();
     }    
 }
--- Astrorunners/TempActor.java
+++ Astrorunners/TempActor.java
@@ -1,23 +1,121 @@
 import greenfoot.*; 
 public class TempActor extends Actor
 {
-     private static int XCord;
-    private static int YCord;
-    public int getXCord(){
-      return XCord;    
+    private boolean grounded, wHeld;
+    private int xSpeed, ySpeed, xSpeedMax, ySpeedMax, frame;
+    public TempActor() {
+        xSpeedMax = 10;
+        ySpeedMax = 20;
     }
-    public void SetXCord(){
-        
-    }
-     
+
     public void act() 
-    {  
-        int gravity = 5;
-        if(Greenfoot.isKeyDown("E")){setLocation(getX(),getY()-30);}
-        if(Greenfoot.isKeyDown("D")){setLocation(getX()+5, getY());}
-        if(Greenfoot.isKeyDown("A")){setLocation(getX()-5,getY());}
-        setLocation(getX(), getY()+gravity);
-        if(isTouching(Platforms.class)){setLocation(getX(),getY()-gravity);}
-        //I'm going to keep myself safe :3
-    }   
+    {
+        frame++;
+        // ARE WE GROUNDED?
+        grounded = onGroundCheck();
+        // APPLY GRAVITY
+        ySpeed++;
+        // CHECK KEY PRESSES
+        if(Greenfoot.isKeyDown("W") && grounded){
+            ySpeed = -20;
+            wHeld = true;
+        }
+        // RELEASE JUMP
+        if(!Greenfoot.isKeyDown("W") && wHeld && ySpeed < 0) {
+            ySpeed = ySpeed / 2;
+            wHeld = false;
+        }
+        // RUN LEFT/RIGHT
+        if(Greenfoot.isKeyDown("D")){xSpeed++;}
+        if(Greenfoot.isKeyDown("A")){xSpeed--;}
+        // APPLY FRICTION
+        if(frame % 2 == 0) {
+            if(xSpeed > 0) {xSpeed--;}
+            if(xSpeed < 0) {xSpeed++;}
+        }
+        // CAP THEIR SPEEDS
+        if(xSpeed < -xSpeedMax) {xSpeed = -xSpeedMax;}
+        if(xSpeed > xSpeedMax) {xSpeed = xSpeedMax;}
+        if(ySpeed < -ySpeedMax) {ySpeed = -ySpeedMax;}
+        if(ySpeed > ySpeedMax) {ySpeed = ySpeedMax;}
+        // UPDATE LOCATION BASED ON SPEEDS
+        setLocation(getX()+xSpeed, getY()+ySpeed);
+        // FIX POSITION BASED ON COLLISIONS
+        collisions();
+    }
+
+    public boolean onGroundCheck() {
+        int w = getImage().getWidth();
+        int h = getImage().getHeight();
+        Actor BL = getOneObjectAtOffset(-w/4,  h/2, Platforms.class);
+        if(BL != null) {
+            return true;
+        }
+        Actor BR = getOneObjectAtOffset( w/4,  h/2, Platforms.class);
+        if(BR != null){
+            return true;
+        }
+        return false;
+    }
+
+    public void collisions() {
+        int w = getImage().getWidth();
+        int h = getImage().getHeight();
+        Actor BL = getOneObjectAtOffset(-w/4,  h/2, Platforms.class);
+        if(BL != null) {
+            int ow = BL.getImage().getWidth();
+            int oh = BL.getImage().getHeight();
+            setLocation(getX(), BL.getY()-oh/2-h/2);
+            ySpeed = 0;
+        }
+        Actor BR = getOneObjectAtOffset( w/4,  h/2, Platforms.class);
+        if(BR != null){
+            int ow = BR.getImage().getWidth();
+            int oh = BR.getImage().getHeight();
+            setLocation(getX(), BR.getY()-oh/2-h/2);
+            ySpeed = 0;
+        }
+        Actor TL = getOneObjectAtOffset(-w/4, -h/2, Platforms.class);
+        if(TL != null){
+            int ow = TL.getImage().getWidth();
+            int oh = TL.getImage().getHeight();
+            setLocation(getX(), TL.getY()+oh/2+h/2);
+            ySpeed = 0;
+        }
+        Actor TR = getOneObjectAtOffset( w/4, -h/2, Platforms.class);
+        if(TR != null){
+            int ow = TR.getImage().getWidth();
+            int oh = TR.getImage().getHeight();
+            setLocation(getX(), TR.getY()+oh/2+h/2);
+            ySpeed = 0;
+        }
+        Actor LT = getOneObjectAtOffset(-w/2, -h/4, Platforms.class);
+        if(LT != null){
+            int ow = LT.getImage().getWidth();
+            int oh = LT.getImage().getHeight();
+            setLocation(LT.getX()+ow/2+w/2, getY());
+            xSpeed = 0;
+        }
+        Actor LB = getOneObjectAtOffset(-w/2,  h/4, Platforms.class);
+        if(LB != null){
+            int ow = LB.getImage().getWidth();
+            int oh = LB.getImage().getHeight();
+            setLocation(LB.getX()+ow/2+w/2, getY());
+            xSpeed = 0;
+        }
+        Actor RT = getOneObjectAtOffset( w/2, -h/4, Platforms.class);
+        if(RT != null){
+            int ow = RT.getImage().getWidth();
+            int oh = RT.getImage().getHeight();
+            setLocation(RT.getX()-ow/2-w/2, getY());
+            xSpeed = 0;
+        }
+        Actor RB = getOneObjectAtOffset( w/2,  h/4, Platforms.class);
+        if(RB != null){
+            int ow = RB.getImage().getWidth();
+            int oh = RB.getImage().getHeight();
+            setLocation(RB.getX()-ow/2-w/2, getY());
+            xSpeed = 0;
+        }
+    }
 }
