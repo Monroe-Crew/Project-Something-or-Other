e0c3a744acb17a55e9086b1f5394ddcbb22913c6
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-11-16 14:32:13 -0600

    Basic astronaut instantiation

--- Astrorunners/MainGame.java
+++ Astrorunners/MainGame.java
@@ -6,8 +6,7 @@ public class MainGame extends World{
     }
 
     private void prepare(){
-        //Astronaut astronaut = new Astronaut();
-        //addObject(astronaut,100,400);
+        // Constructs stage
         BlackHole blackHole = new BlackHole();
         addObject(blackHole,602,411);
 
@@ -16,6 +15,11 @@ public class MainGame extends World{
         /**
          * TODO: IMPLEMENT DETERMINATION OF NO. OF PLAYERS
          */
+        
+        Astronaut blueAstronaut = new Astronaut();
+        Astronaut greenAstronaut = new Astronaut();
+        // Astronaut 3
+        // Astronaut 4
 
         PlayerScore player1Score = new PlayerScore(" ");
         PlayerScore player2Score = new PlayerScore(" ");
@@ -24,15 +28,26 @@ public class MainGame extends World{
 
         switch(numPlayers){
             case 4:
+            //Instantiates score for 3rd and 4th player
             addObject(player4Score, 0, 0);
             addObject(player3Score, 0, 0);
+            //Instantiates 3rd and 4th player astronauts
+            //addObject(astronaut4)
+            //addObject(astronaut3)
             
             case 3:
+            //Instantiates score for 3rd player
             addObject(player3Score, 0, 0);
+            //addObject(astronaut3)
             
             default:
+            //Instantiates score for 1st and 2nd player
             addObject(player2Score, 0, 0);
             addObject(player1Score, 0, 0);
+            //Instantiates 2 player astronauts
+            addObject(blueAstronaut, 0, 0);
+            addObject(greenAstronaut, 0, 0);
+            
         }
     }
 }
