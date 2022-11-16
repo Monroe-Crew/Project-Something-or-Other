563311b0464bff50d5e559dca9a08c7901f1fc76
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-11-16 14:06:08 -0600

    Nukes the TestWorld + ports over score components

--- Astrorunners/MainGame.java
+++ Astrorunners/MainGame.java
@@ -10,5 +10,23 @@ public class MainGame extends World{
         //addObject(astronaut,100,400);
         BlackHole blackHole = new BlackHole();
         addObject(blackHole,602,411);
+        
+        int numPlayers;
+        
+        PlayerScore player1Score = new PlayerScore("");
+        PlayerScore player2Score = new PlayerScore("");
+        PlayerScore player3Score = new PlayerScore("");
+        PlayerScore player4Score = new PlayerScore("");
+        
+        switch(numPlayers){
+            case 4:
+                addObject(player4Score, 0, 0);
+                addObject(player3Score, 0, 0);
+            case 3:
+                addObject(player3Score, 0, 0);
+            default:
+                addObject(player2Score, 0, 0);
+                addObject(player1Score, 0, 0);
+        }
     }
 }
