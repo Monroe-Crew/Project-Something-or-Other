4baf2cccb4706906ae9d8dff1c95e9458bd7e87c
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-11-16 21:34:33 -0600

    Sound Effect Implementation Pt. 1

--- Astrorunners/Start_Button.java
+++ Astrorunners/Start_Button.java
@@ -5,6 +5,7 @@ public class Start_Button extends Actor
     public void act()
     {
         if(Greenfoot.mouseClicked(this)){
+			Greenfoot.playSound("GameBegin.mp3");
             Greenfoot.setWorld(new MainGame());
         }
     }
