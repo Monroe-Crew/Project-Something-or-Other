ec6270deb754fcd2cd2d76a5a1586facfbf384e9
Em1lisGod42069 <urmomsboyfriend42069@gmail.com>
2022-11-17 12:19:24 -0600

    update in score

--- Astrorunners/Score.java
+++ Astrorunners/Score.java
@@ -3,9 +3,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Score extends Actor
 {
-
     public void Score(int PlayID, int score){
-        
+        GreenfootImage scoreString = new GreenfootImage (300,100);
+        scoreString.drawString("Player "+PlayID+" Score: "+ score,200,200);
+        setImage(scoreString); 
     }
     public void act()
     {
