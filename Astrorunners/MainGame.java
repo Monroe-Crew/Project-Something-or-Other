4950c278ea306471461cac9469d2d956e3cc3593
CadenWonzer <caden.wonzer@icloud.com>
2022-11-16 23:35:24 -0600

    Fixed Player Spawn
    player spawning added

--- Astrorunners/MainGame.java
+++ Astrorunners/MainGame.java
@@ -15,27 +15,27 @@ public class MainGame extends World{
         for(int i = 0; i < numPlayers; i++){
             if(playersJoined.get(i) == 1){
                 Astronaut blueAstronaut = new Astronaut(1);
-                addObject(blueAstronaut,150,290);
+                addObject(blueAstronaut,125,192);
                 Platform2 blueStart = new Platform2();
-                addObject(blueStart,175,290);
+                addObject(blueStart,175,192);
             }
             else if(playersJoined.get(i) == 2){
                 Astronaut greenAstronaut = new Astronaut(2);
-                addObject(greenAstronaut,150,480);
+                addObject(greenAstronaut,125,384);
                 Platform2 greenStart = new Platform2();
-                addObject(greenStart,175,480);
+                addObject(greenStart,175,384);
             }
-            else if(playersJoined.get(i) == 2){
+            else if(playersJoined.get(i) == 3){
                 Astronaut pinkAstronaut = new Astronaut(3);
-                addObject(pinkAstronaut,150,670);
+                addObject(pinkAstronaut,125,576);
                 Platform2 pinkStart = new Platform2();
-                addObject(pinkStart,175,670);
+                addObject(pinkStart,175,576);
             }
-            else if(playersJoined.get(i) == 2){
+            else if(playersJoined.get(i) == 4){
                 Astronaut yellowAstronaut = new Astronaut(4);
-                addObject(yellowAstronaut,150,860);
+                addObject(yellowAstronaut,125,768);
                 Platform2 yellowStart = new Platform2();
-                addObject(yellowStart,175,860);
+                addObject(yellowStart,175,768);
             }
         }    
         Goal goal = new Goal();
