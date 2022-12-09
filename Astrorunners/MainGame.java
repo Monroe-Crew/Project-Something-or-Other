45d8b06a02710529243fc1e7f73a897c8197ab35
CadenWonzer <97636285+CadenWonzer@users.noreply.github.com>
2022-12-09 14:17:07 -0600

    Added All obejects to altPrepare for after round screen

--- Astrorunners/MainGame.java
+++ Astrorunners/MainGame.java
@@ -11,7 +11,7 @@ public class MainGame extends World{
     }
 
     public MainGame(int round, List<Astronaut> allPlayers){    
-        super(Constants.WIDTH, Constants.HEIGHT, 1); 
+        super(Constants.WIDTH, Constants.HEIGHT, 1, false); 
         round++;
         this.round = round;
         altPrepare(allPlayers);
@@ -75,35 +75,54 @@ public class MainGame extends World{
 
     private void altPrepare(List<Astronaut> allPlayers){ //takes in a list of the astronauts for after round updates.
         BlackHole blackHole = new BlackHole();
-        addObject(blackHole,602,411);
+        addObject(blackHole,640,480);
 
         for(int i = 0; i < allPlayers.size(); i++){
             int playerID = allPlayers.get(i).getPlayerID();
             if(playerID == 1){
-                addObject(allPlayers.get(i),125,192);
+                addObject(allPlayers.get(i),182,212);
                 Platform2 blueStart = new Platform2();
-                addObject(blueStart,175,192);
+                addObject(blueStart,216,235);
             }
             else if(playerID == 2){
-                addObject(allPlayers.get(i),125,384);
+                addObject(allPlayers.get(i),119,340);
                 Platform2 greenStart = new Platform2();
-                addObject(greenStart,175,384);
+                addObject(greenStart,167,363);
             }
             else if(playerID == 3){
-                addObject(allPlayers.get(i),125,576);
+                addObject(allPlayers.get(i),105,630);
                 Platform2 pinkStart = new Platform2();
-                addObject(pinkStart,175,576);
+                addObject(pinkStart,167,607);
             }
             else if(playerID == 4){
-                addObject(allPlayers.get(i),125,768);
+                addObject(allPlayers.get(i),182,768);
                 Platform2 yellowStart = new Platform2();
-                addObject(yellowStart,175,768);
+                addObject(yellowStart,216,744);
             }
         }
         List<Integer> playersJoined = new ArrayList<Integer>();
         for(int i = 0; i < allPlayers.size(); i++){
             playersJoined.add(allPlayers.get(i).getPlayerID());
         }
+        
+        Platform1 platform1 = new Platform1();
+        addObject(platform1, 415, 480);
+        Platform1 platform2 = new Platform1();
+        addObject(platform2, 640, 255);
+        Platform1 platform3 = new Platform1();
+        addObject(platform3, 640, 705);
+        Platform1 platform4 = new Platform1();
+        addObject(platform4, 481, 321);
+        Platform1 platform5 = new Platform1();
+        addObject(platform5, 481, 639);
+        Platform1 platform6 = new Platform1();
+        addObject(platform6, 799, 321);
+        Platform1 platform7 = new Platform1();
+        addObject(platform7, 799, 639);
+        
+        Meteoroid meteoroid = new Meteoroid(500, -2.5, 1520, 480, 3);
+        addObject(meteoroid, 1045, 480);
+        
         Goal goal = new Goal(allPlayers.size(), playersJoined, round);
         addObject(goal,1186,409);
     }
