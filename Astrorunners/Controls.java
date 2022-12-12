19afa1a533175702109bdf65073e8ec6cf867b32
Alexander <47544120+sunzu432@users.noreply.github.com>
2022-12-11 23:38:38 -0600

    Control screen world + implementation

---
+++ Astrorunners/Controls.java
@@ -0,0 +1,30 @@
+import greenfoot.*;
+import java.util.Random;
+import java.util.List;
+
+public class Controls extends World
+{
+    private int frame = 0;
+    Random rand = new Random();
+    private List<Integer> players;
+    public Controls(List<Integer> playersJoined)
+    {    
+        super(Constants.WIDTH, Constants.HEIGHT, 1); 
+        setBackground("Controls Screen.png");
+        players = playersJoined;
+    }
+
+    public void act(){
+        frame++;
+        if(frame == 3600){
+            switch(rand.nextInt(3)){
+                case 2:
+                    Greenfoot.setWorld(new MeteoroidGame(players, 0));
+                case 1:
+                    Greenfoot.setWorld(new CometGame(players, 0));
+                default:
+                    Greenfoot.setWorld(new MainGame(players, 0));
+            }
+        }
+    }
+}
